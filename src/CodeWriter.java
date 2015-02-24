87452f36ab2001ac465ba804e2ee095dc46e9f84
Gokul Natesan <gnatesan@mines.edu>
2015-02-23 23:11:17 -0700

    2nd Commit
    Able to successfully write corresponding Hack Assembly code to a .asm
    file when trying to push constants in VM language onto stack

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -1,4 +1,64 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CodeWriter {
+	FileWriter fw;
+	
+	public CodeWriter(File outfile) throws IOException {
+		fw = new FileWriter(outfile);
+	}
 
+	public void setFileName(String fileName) {
+		
+	}
+	
+	public void writeArithmetic(String command) {
+		
+	}
+	
+	public void WritePushPop(String PushPop, String segment, int index) throws IOException {
+		if (PushPop.equals("C_PUSH")) {
+			pushCommand(segment, index);
+		}
+		else 
+			popCommand(segment, index);
+	}
+	
+	public void pushCommand(String segment, int index) throws IOException {
+		if (segment.equals("constant")) {
+			fw.write("@" + Integer.toString(index));
+			fw.write(System.lineSeparator());
+			fw.write("D = A");
+			fw.write(System.lineSeparator());
+			fw.write("@SP");
+			fw.write(System.lineSeparator());
+			fw.write("A = M");
+			fw.write(System.lineSeparator());
+			fw.write("M = D");
+			fw.write(System.lineSeparator());
+			fw.write("@SP");
+			fw.write(System.lineSeparator());
+			fw.write("M = M + 1");
+			fw.write(System.lineSeparator());
+		}
+	}
+	
+	public void popCommand(String segment, int index) throws IOException {
+		
+	}
+	
+	
+	public void Close() throws IOException {
+		fw.close();
+	}
+	
+	/*public static void main (String[] args) {
+		try {
+			CodeWriter cw = new CodeWriter("out.asm");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
 }
--- src/Parser.java
+++ src/Parser.java
@@ -1,43 +1,104 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Parser {
 	
 	File file;
 	Scanner in;
+	String currentCommand;
+	String commandType;
+	String argument1;
+	int argument2;
+	StringTokenizer st;
+	HashMap <String, String> allCommands;
+	CodeWriter cw;
+	
 	
 	public Parser(String filename) {
 		file = new File(filename);
 		try {
 			in = new Scanner(file);
-		} catch (FileNotFoundException e) {
+			cw = new CodeWriter(new File("out.asm"));
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		allCommands = new HashMap <String, String>();
+		allCommands.put("push", "C_PUSH");
+		allCommands.put("pop", "C_POP");
+		allCommands.put("add", "C_ARITHMETIC");
+		allCommands.put("sub", "C_ARITHMETIC");
+		allCommands.put("neg", "C_ARITHMETIC");
+		allCommands.put("eq", "C_ARITHMETIC");
+		allCommands.put("gt", "C_ARITHMETIC");
+		allCommands.put("lt", "C_ARITHMETIC");
+		allCommands.put("and", "C_ARITHMETIC");
+		allCommands.put("or", "C_ARITHMETIC");
+		allCommands.put("not", "C_ARITHMETIC");
+		allCommands.put("label", "C_LABEL");
+		allCommands.put("function", "C_FUNCTION");
+		allCommands.put("return", "C_RETURN");
+		allCommands.put("goto", "C_GOTO");
+		allCommands.put("if-goto", "C_IF");
 	}
 	
 	public boolean hasMoreCommands() {
-		return true;
+		if (in.hasNext() && in.hasNextLine()) {
+			return true;
+		}
+		return false;
 	}
 	
 	public void advance() {
-		
+		if (hasMoreCommands()) {
+			this.currentCommand = in.nextLine();
+			//System.out.println(currentCommand);
+		}
+
 	}
 	
 	public String commandType() {
-		return "";
+		st = new StringTokenizer(currentCommand);
+		return allCommands.get(st.nextToken());
 	}
 	
 	public String arg1() {
-		return "";
+		st = new StringTokenizer(currentCommand);
+		st.nextToken();
+		return st.nextToken();
 	}
 	
 	public int arg2() {
-		return 0;
+		st = new StringTokenizer(currentCommand);
+		st.nextToken();
+		st.nextToken();
+		return Integer.parseInt(st.nextToken());
 	}
 	
-public static void main (String[] args) {
-		
+	public void writeOutput() throws IOException {
+		commandType = commandType();
+		argument1 = arg1();
+		if (commandType.equals("C_PUSH") || commandType.equals("C_POP")) {
+			argument2 = arg2();
+			cw.WritePushPop(commandType, argument1, argument2);
+		}
+	}
+	
+	public void close() throws IOException {
+		cw.Close();
+	}
+	
+	public static void main (String[] args) throws IOException {
+		Parser p = new Parser("test.txt");
+		while (p.hasMoreCommands()) {
+			p.advance();
+			p.writeOutput();
+		}
+		p.close();
 	}
 }
